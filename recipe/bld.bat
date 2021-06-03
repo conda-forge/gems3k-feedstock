6e024b46670c37c6e8b26cd64f9968c1e01eebc4
George Dan Miron <44091914+gdmiron@users.noreply.github.com>
2021-06-03 17:24:12 +0200

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,11 +1,12 @@
 mkdir build
 cd build
 
-cmake -DCMAKE_BUILD_TYPE=Release ^
+cmake -G Ninja ^
+      -DCMAKE_BUILD_TYPE=Release ^
       -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
       -DCMAKE_INCLUDE_PATH:PATH="%LIBRARY_INC%" ^
       ..
 if errorlevel 1 exit 1
-make
-make install
+ninja -j 1
+ninja install
 if errorlevel 1 exit 1
