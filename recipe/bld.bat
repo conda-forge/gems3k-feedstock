6d5a7b7ec2fb04bc6b207f641222c2fbda8813b6
svetad <sv.dmytr@gmail.com>
2025-12-15 17:41:26 +0200

    change meta

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,11 +2,17 @@ mkdir build
 cd build
 
 cmake -G Ninja ^
+      -DSOLMOD4RKT_PYTHON_INSTALL_PREFIX="%PREFIX%" ^
       -DCMAKE_BUILD_TYPE=Release ^
       -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
       -DCMAKE_INCLUDE_PATH:PATH="%LIBRARY_INC%" ^
+      -DPython_EXECUTABLE="%PYTHON%" ^
+      -DPython_INCLUDE_DIR="%PYTHON_INCLUDE%" ^
+      -DPython_LIBRARY="%PYTHON_LIB%" ^
       ..
+
 if errorlevel 1 exit 1
 ninja -j 1
 ninja install
 if errorlevel 1 exit 1
+
